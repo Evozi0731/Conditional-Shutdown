a43ea81d4aacbf6c43fe5248bd1b37e5de3b54ec
YEET <51841285+Evozi0731@users.noreply.github.com>
2020-02-16 18:57:33 +0900

    Create cs_ver01.py

---
+++ cs_ver01.py
@@ -0,0 +1,35 @@
+import datetime as dt
+import os
+import time
+
+
+def conditional_shutdown():
+    while True:
+        time.sleep(60)
+        day = dt.date.today().isoweekday()
+        timenow = dt.datetime.now().hour
+        if day == 5:
+          if timenow < 5:
+              os.system('shutdown -s -t 1')
+          else:
+              pass
+        elif day == 6 or day == 7:
+            if timenow < 5:
+                os.system('shutdown -s -t 1')
+            else:
+                pass
+        elif day == 7:
+            if timenow >= 23:
+                os.system('shutdown -s -t 1')
+            else:
+                pass
+        else:
+            if timenow >= 23 or timenow < 5:
+                os.system('shutdown -s -t 1')
+            else:
+                print('You\'re clear')
+                pass
+        time.sleep(540)
+
+
+conditional_shutdown()
